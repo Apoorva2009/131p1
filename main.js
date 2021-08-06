adde55f56f32e1de1d4adb37b4145e7da4340c23
Apoorva thakur <74960288+Apoorva2009@users.noreply.github.com>
2021-08-06 19:26:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+img="";
+status="";
+
+function preload(){
+img=loadImage('p.jpg');
+}
+
+function setup(){
+canvas=createCanvas(640,420);
+canvas.center();
+}
+
+
+function draw(){
+image(img,0,0,640,420);
+
+}
